b350f8d2469f3b1c191a40813baa490f62722040
Alexander Madoff <a1madoff@gmail.com>
2020-05-01 18:07:10 -0400

    testing

--- code/hyperparameters2.py
+++ code/hyperparameters2.py
@@ -4,8 +4,10 @@ style_loss_weight = 0.005
 temporal_loss_weight = 1
 
 # img dimensions
-img_height = 168
-img_width = 299
+# img_height = 168
+# img_width = 299
+img_height = 224
+img_width = 224
 
 # learning rate for gradient descent
 learning_rate = 0.04
