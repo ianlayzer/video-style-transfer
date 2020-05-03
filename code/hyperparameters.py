026e6b64e112a3797c004b9e85cfa365fbded647
gvannewk <57049370+gvannewk@users.noreply.github.com>
2020-05-02 20:08:52 -0700

    last layer

--- code/hyperparameters.py
+++ code/hyperparameters.py
@@ -6,14 +6,14 @@ style_loss_weight = 0.5
 temporal_loss_weight = 1
 
 # img dimensions
-# img_height = 558
-# img_width = 700
-img_height = 224
-img_width = 224
+img_height = 558
+img_width = 700
+# img_height = 224
+# img_width = 224
 
 # layer weights for style loss assuming 5 style layers
 
-style_layer_weights = [5, 0, 0, 0, 0]
+style_layer_weights = [0, 0, 0, 0, 5]
 
 # learning rate for gradient descent
 learning_rate = 0.04
