2b2eb533088d82502019396d5b2381c5420f84f3
Eyal Levin <60202457+babajaj@users.noreply.github.com>
2020-05-03 16:28:39 -0700

    Update main.py

--- code/main.py
+++ code/main.py
@@ -7,8 +7,18 @@ from stylize import stylize_image
 from stylize import stylize_video
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 data_folder = os.path.dirname(__file__) + '../data/'
+framerate = 30
+video_path = "./../data/content/video/elephant.mp4"
+# # style_path = tf.keras.utils.get_file('kandinsky.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
+
+# # content_path = tf.keras.utils.get_file('Labrador.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
+
+image_path = "./../data/content/images/Labrador.jpg"
+style_path = "./../data/style/Starry_Night.jpg"
+
+
+
 
 def parse_args():
     """ Perform command-line argument parsing. """
@@ -26,16 +36,6 @@ def parse_args():
         action="store_true",
         help='''are you loading in an image?''')
     parser.add_argument(
-        '--content',
-        required=True,
-        type=str,
-        help='''the content file''')   
-    parser.add_argument(
-        '--style',
-        required=True,
-        type=str,
-        help='style file.')
-    parser.add_argument(
         '--both',
         required=False,
         action="store_true",
@@ -51,23 +51,19 @@ def parse_args():
 
 def main():
     #get image paths
-    content_path = data_folder + ARGS.content
-    style_path = data_folder + ARGS.style
     #calling img_stylize or vid_stylize to stylize the content
     if ARGS.image:
         print(True)
-        stylize_image(content_path, style_path)
+        stylize_image(image_path, style_path)
     if ARGS.video:
         print(False)
-        stylize_video(content_path, style_path, 30)
+        stylize_video(video_path, style_path, framerate)
         #idk what the actual video func is called
         #stylize_video(content_path, style_path)
         
     
         
     
-    
-
 
 #image vs. video, style, content, temporal loss (none, short, both)
 #global arguments
