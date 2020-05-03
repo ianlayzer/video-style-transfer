c9a4a8d0cfab7d58f19e017ed4b73272a4b546ca
gvannewk <57049370+gvannewk@users.noreply.github.com>
2020-05-03 16:38:14 -0700

    notshrot

--- code/run.py
+++ code/run.py
@@ -1,6 +1,6 @@
 from stylize import stylize_image, stylize_video
 
-video_path = "./../data/content/video/tom_jerry_short.mp4"
+video_path = "./../data/content/video/tom_jerry.mp4"
 # # style_path = tf.keras.utils.get_file('kandinsky.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
 
 # # content_path = tf.keras.utils.get_file('Labrador.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
