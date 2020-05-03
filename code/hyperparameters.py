01f14c65d93fba6aaf8251c72c30b266b9ccdd50
gvannewk <57049370+gvannewk@users.noreply.github.com>
2020-05-02 23:04:30 -0700

    eyal weights

--- code/hyperparameters.py
+++ code/hyperparameters.py
@@ -1,7 +1,7 @@
 # weights for content and style loss (α, β, γ in Ruder et al.)
 # content_loss_weight = 30000
-content_loss_weight = 10000000
-style_loss_weight = .000003
+content_loss_weight = 4000000
+style_loss_weight = .001
 temporal_loss_weight = 1
 # img dimensions
 img_height = 558
