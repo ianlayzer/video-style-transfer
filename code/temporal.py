36be4fcfb45507142bde9c6bcaa6bbbaafb1c56e
gvannewk <57049370+gvannewk@users.noreply.github.com>
2020-05-03 21:42:49 -0700

    debugging

--- code/temporal.py
+++ code/temporal.py
@@ -61,6 +61,6 @@ def compute_disocclusion_mask(prev_frame, curr_frame, next_frame):
 
 	mask = tf.expand_dims(mask, 0)
 
-	mask = tf.broadcast_to(tf.shape(mask)[0], tf.shape(mask)[1], tf.shape(mask)[2], 3)
+	mask = tf.broadcast_to(mask, (tf.shape(mask)[0], tf.shape(mask)[1], tf.shape(mask)[2], 3))
 
 	return mask
