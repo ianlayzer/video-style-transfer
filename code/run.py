a8299ec6c36271362e3d2c7065792179f7583b6f
gvannewk <57049370+gvannewk@users.noreply.github.com>
2020-05-03 18:30:39 -0700

    0 frames

--- code/run.py
+++ code/run.py
@@ -1,4 +1,5 @@
 from stylize import stylize_image, stylize_video
+import hyperparameters as hp
 
 video_path = "./../data/content/video/tom_jerry.mp4"
 # # style_path = tf.keras.utils.get_file('kandinsky.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
@@ -8,4 +9,4 @@ video_path = "./../data/content/video/tom_jerry.mp4"
 content_path = "./../data/content/images/Labrador.jpg"
 style_path = "./../data/style/Starry_Night.jpg"
 
-stylize_video(video_path, style_path, fps=1)
+stylize_video(video_path, style_path, 1, hp.content_loss_weight, hp.style_loss_weight, hp.temporal_loss_weight, hp.num_epochs, hp.learning_rate)
