b361ec39fbb8a8d1cbbab37d3770714145bdd9bc
gvannewk <57049370+gvannewk@users.noreply.github.com>
2020-05-06 00:30:07 -0700

    lkj

--- code/run.py
+++ code/run.py
@@ -10,4 +10,4 @@ image_path = "./../data/content/images/elephant.jpg"
 style_path = "./../data/style/water-lilies.jpg"
 
 # stylize_image(image_path, style_path, hp.content_loss_weight, hp.style_loss_weight, hp.temporal_loss_weight, hp.learning_rate, hp.num_epochs)
-stylize_video(video_path, style_path, 29, hp.content_loss_weight, hp.style_loss_weight, hp.temporal_loss_weight, hp.num_epochs, hp.learning_rate, True)
+stylize_video(video_path, style_path, 29, hp.content_loss_weight, hp.style_loss_weight, hp.temporal_loss_weight, hp.num_epochs, hp.learning_rate, False)
