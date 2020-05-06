6ca7e57640df5363ca7a4f64048eb028c4f8297a
gvannewk <57049370+gvannewk@users.noreply.github.com>
2020-05-05 22:26:41 -0700

    sdf

--- code/hyperparameters.py
+++ code/hyperparameters.py
@@ -1,13 +1,13 @@
 # weights for content and style loss (α, β, γ in Ruder et al.)
 # content_loss_weight = 30000
-content_loss_weight = 10000
+content_loss_weight = 1
 style_loss_weight = .03
-temporal_loss_weight = 100000000
+temporal_loss_weight = 4000000000
 # img dimensions
 img_height = 216
 img_width = 384
 # layer weights for style loss assuming 5 style layers
-style_layer_weights = [1, 1, 1, 1, 1]
+style_layer_weights = [1, 0, 0, 0, 0]
 
 # learning rate for gradient descent
 learning_rate = 0.04
